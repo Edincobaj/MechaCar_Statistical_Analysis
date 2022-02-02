55197892d1e51d71018e648401489e9d1515b02b
Edincobaj <41974323+Edincobaj@users.noreply.github.com>
2022-02-02 16:26:46 -0500

    Added deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,5 +10,11 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Use the summary function to determine p-value and the r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar))
 
+# Read in Suspension_Coil.csv file as a dataframe
+Suspension_Coil <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 
+# Create a total_summary dataframe
+total_summary <- Suspension_Coil %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
 
+# Create a lot_summary dataframe grouping each manufacturing lot.
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups='keep')
